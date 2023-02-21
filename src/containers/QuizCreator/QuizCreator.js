2ed926719bdd3282d64d9852700fa9e59b7d6170
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2023-02-21 20:29:13 +0200

    Add Select.js component
    This adds component which returns a custom select element. Furthermore
    added styles for this component. In the QuizCreator component changed
    the logic of input controls.

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,20 +3,31 @@ import classes from "./QuizCreator.module.scss"
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import FormControls from "../../form/fromFramework";
-// ! testing
-const test = new FormControls("email", "email", true, true)
-test.setRules({ email: "email", minLength: 6 })
-console.log(test);
+import Select from "../../components/UI/Select/Select";
+
+// * input controls
+const addQuestionInputControls = new FormControls("text", "enter a question", true, true)
+addQuestionInputControls.setErrorMessage("This field can't be empty")
+
+function createOptionControls(number) {
+    const addAnswerInputControls = new FormControls("text", `answer ${number}`, true, true)
+    addAnswerInputControls.setErrorMessage("This value is required")
+    addAnswerInputControls.id = number
+    return addAnswerInputControls
+}
+
+// TODO: add function or reference which return basic controls (reset controls)
 
 function QuizCreator() {
     const [quiz, setQuiz] = useState([]);
+    const [rightAnswerId, setRightAnswerId] = useState(1);
     const [formControls, setFormControls] = useState(
         {
-            question: '',
-            option1: '',
-            option2: '',
-            option3: '',
-            option4: '',
+            question: addQuestionInputControls,
+            option1: createOptionControls(1),
+            option2: createOptionControls(2),
+            option3: createOptionControls(3),
+            option4: createOptionControls(4),
         }
     );
 
@@ -32,19 +43,48 @@ function QuizCreator() {
         console.log(`You click ${target}`);
     }
 
+    const onInputChangeHandler = (value, controlName) => {
+        console.log(value);
+    }
+
+    const onSelectChangeHandler = ({ target }) => {
+        console.log(target.value);
+        setRightAnswerId(+target.value)
+    }
+
+    const renderInputs = () => {
+        return Object.keys(formControls).map((controlName, i) => {
+            const { value, type, label, validation } = formControls[controlName]
+            return (
+                <>
+                    <Input
+                        key={i}
+                        value={value}
+                        type={type}
+                        label={label}
+                        onChangeHandler={(e) => onInputChangeHandler(e.target.value, controlName)}
+                        validation={validation}
+                    />
+                    {i === 0 ? <hr /> : null}
+                </>
+            )
+        })
+    }
+
     return (
         <div className={classes.QuizCreator}>
             <div className={classes.QuizCreator__innerFlexContainer}>
                 <h1 className={classes.QuizCreator__title}>Create quiz</h1>
                 <form className={classes.QuizCreator__form} onSubmit={onFormSubmitHandler}>
-                    <input type="text" />
-                    <hr />
 
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
+                    {renderInputs()}
 
-                    <select></select>
+                    <Select
+                        label={'Label'}
+                        value={rightAnswerId}
+                        options={[{ value: 1, text: "first val" }, { value: 2, text: "second val" }, { value: 3, text: "last val" }]}
+                        onChangeHandler={onSelectChangeHandler}
+                    />
 
                     <div className={classes.QuizCreator__formControls}>
                         <Button type="primary" onClick={addQuestionHandler}>Add question</Button>
