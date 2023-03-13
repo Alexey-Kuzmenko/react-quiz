fd8c3e1f37663f87a760d6554808ef3d93e2e265
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2023-03-13 19:04:41 +0200

    Add adaptation for mobile devices
    Now all app components are adapted for mobile devices. Furthermore added a file
    with styles variables.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -4,8 +4,11 @@ import MenuToggle from "../../components/Nav/MenuToggle/MenuToggle";
 import Drawer from "../../components/Nav/Drawer/Drawer";
 import { Outlet } from "react-router-dom";
 
+const body = document.body
+
 function Layout() {
     const [menuIsOpen, setMenuIsOpen] = useState(false);
+    body.dataset.bodyScroll = !menuIsOpen
 
     const onMenuToggleClickHandler = (e) => {
         setMenuIsOpen(!menuIsOpen)
