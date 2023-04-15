dbbcaea00e4799bafe7e1d2227790ed77a266e82
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2023-04-15 11:32:30 +0300

    Сorrected syntax mistakes

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,5 @@
 const apiConfig = {
     url: 'https://react-quiz-game-ee138-default-rtdb.europe-west1.firebasedatabase.app',
-    // ! remove
-    // signUpApi: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
-    // signInApi: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
     authApi: 'https://identitytoolkit.googleapis.com/v1',
     apiKey: 'AIzaSyAmn2gjQHJFz-gSSLh-QXBsrf87eggyTeQ'
 }
